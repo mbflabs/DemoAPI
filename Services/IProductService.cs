a0e38fa3ad65efc1d8be4b341f37bbb551fed820
mbflabs <159748185+mbflabs@users.noreply.github.com>
2026-04-21 08:16:01 +0700

    Create IProductService.cs

---
+++ Services/IProductService.cs
@@ -0,0 +1,12 @@
+using DemoAPI.Models;
+
+namespace DemoAPI.Services;
+
+public interface IProductService
+{
+    Task<IEnumerable<Product>> GetAllAsync();
+    Task<Product?> GetByIdAsync(int id);
+    Task<Product> CreateAsync(Product product);
+    Task<Product?> UpdateAsync(int id, Product product);
+    Task<bool> DeleteAsync(int id);
+}
