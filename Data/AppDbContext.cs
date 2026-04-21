1870154ae914a8166bdcc75aa502c2ee6ef96773
mbflabs <159748185+mbflabs@users.noreply.github.com>
2026-04-21 08:14:52 +0700

    Create AppDbContext.cs

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using DemoAPI.Models;
+
+namespace DemoAPI.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<Product> Products { get; set; }
+}
